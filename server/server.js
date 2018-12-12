dcf378eeedf080a3f3894cb942301f9f300f1481
github username <github email address>
2018-12-11 20:02:47 -0500

    12/11/18 8:02PM

--- server/server.js
+++ server/server.js
@@ -1,13 +1,40 @@
+// https://intense-stream-91987.herokuapp.com/
+
 const path = require('path');
+const http = require('http')
 const express = require('express');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.emit('newMessage', {
+    from: 'Jun',
+    text: 'Hey, what is going on?'
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnect');
+  }) // 'disconnect' on line 19 is event name
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 })
